f063f153c571f76346c7939877c4a7dffbb7fd08
Mylena Amorim <mylenaamorim13@gmail.com>
2021-07-10 16:14:01 -0300

    Adiciona rota para buscar por Tipo de Doações

--- src/controllers/ongsController.js
+++ src/controllers/ongsController.js
@@ -46,7 +46,7 @@ const mostraOng = async (req, res) => {
 
 const mostraOngsPorTipoDoacoes = async (req, res) => {
     const ongs = await Ong.find().populate('tipoDoacoes')
-    const ongsFiltradas = Ong.filter(ong => ong.tipoDoacoes.nomeTipo == '/' + req.body.nomeTipo + '/')
+    const ongsFiltradas = Ong.filter(ong => ong.tipoDoacoes.nomeTipo == '/' + req.params.nomeTipo + '/')
 }
 
 const alteraOng = async (req, res) => {
