4698de2ab20f46965a6d851a791e9fc451935e7f
Mylena Amorim <mylenaamorim13@gmail.com>
2021-07-10 21:38:55 -0300

    ajuste

--- src/controllers/ongsController.js
+++ src/controllers/ongsController.js
@@ -47,7 +47,7 @@ const mostraOng = async (req, res) => {
 
 const mostraOngsPorTipoDoacoes = async (req, res) => {
     const ongs = await Ong.find().populate('tipoDoacoes')
-    const ongsFiltradas = ongs.filter( ong => ong.tipoDoacoes.id == req.params.id )
+    const ongsFiltradas = ongs.filter(ong => ong.tipoDoacoes._id == req.params.id)
 
     return res.status(200).json(ongsFiltradas)
 }
